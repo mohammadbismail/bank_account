97d165229a1ffded3fc1d715960913d1c4440811
mohammadbismail <m.basim.ismail@gmail.com>
2022-10-26 15:51:46 +0300

    init- not ready

---
+++ BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+    private double checkingBalance;
+    private double savingBalance;
+    public static int numOfAccounts = 0;
+
+    // Constructors
+    public BankAccount() {
+
+    }
+
+    // Setters & getters
+}
