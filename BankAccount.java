3c782046cf85aea4023bdb3bf93b70ff31ff2e41
mohammadbismail <m.basim.ismail@gmail.com>
2022-10-27 14:15:44 +0300

    final

--- BankAccount.java
+++ BankAccount.java
@@ -1,12 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BankAccount {
     private double checkingBalance;
     private double savingBalance;
-    public static int numOfAccounts = 0;
+    public static int NumOfAccounts = 0;
+    public static double totalMoney = 0;
 
     // Constructors
-    public BankAccount() {
+    public BankAccount(double myCheckingBalance, double mySavingBalance) {
+        this.checkingBalance = myCheckingBalance;
+        this.savingBalance = mySavingBalance;
+        NumOfAccounts++;
+        totalMoney += myCheckingBalance + mySavingBalance;
+    }
+    // Setters & getters
+    // public int getAccountCount() {
+    // return NumOfAccounts;
+    // }
 
+    public double getCheckingBalance() {
+        return checkingBalance;
     }
 
-    // Setters & getters
+    public double getSavingBalance() {
+        return savingBalance;
+    }
+
+    public void addBalance(double amount, char accountLetter) {
+        // Scanner getInput = new Scanner(System.in);
+        // System.out.print("Please type 'S' to add to Saving balance and 'C' for
+        // checking balance");
+        // char ch = getInput.next().charAt(0);
+        if (accountLetter == 'C' || accountLetter == 'c') {
+            this.checkingBalance += amount;
+        } else if (accountLetter == 'S' || accountLetter == 's') {
+            this.savingBalance += amount;
+        }
+        // getInput.close();
+        totalMoney += amount;
+    }
+
+    public void withDrawMoney(double amount,char accountLetter) {
+        // Scanner getInput = new Scanner(System.in);
+        // System.out.print("Please type 'S' to add to Saving balance and 'C' for checking balance");
+        // char ch = getInput.next().charAt(0);
+        if (accountLetter == 'C' || accountLetter == 'c' && this.checkingBalance >= amount) {
+            this.checkingBalance -= amount;
+        } else if (accountLetter == 'S' || accountLetter == 's' && this.checkingBalance >= amount) {
+            this.savingBalance -= amount;
+        } else {
+            System.out.println("Your balance is not enough to do such withdraw");
+        }
+        // getInput.close();
+        totalMoney -= amount;
+    }
+
+    public void showBalance() {
+        System.out.println("CheckingBalance is: " + this.checkingBalance);
+        System.out.println("SavingBalance is: " + this.savingBalance);
+    }
 }
--- Test.java
+++ Test.java
@@ -1,3 +1,15 @@
-public class Test {
+import javax.crypto.AEADBadTagException;
 
+public class Test {
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(50, 40);
+        BankAccount account2 = new BankAccount(100, 66.5);
+        BankAccount account3 = new BankAccount(20.2, 30.5);
+        BankAccount account4 = new BankAccount(10.3, 40);
+        System.out.println(BankAccount.NumOfAccounts);
+        System.out.println(BankAccount.totalMoney);
+        account1.addBalance(20, 'C');
+        account1.showBalance();
+        account1.withDrawMoney(200, 'c');
+    }
 }
